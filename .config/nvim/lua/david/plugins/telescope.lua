acdab38aea517f49f525c1a49c2f4a9000bf6bb1
David Kristiansen <david.kristiansen@sony.com>
2024-02-12 15:30:24 +0100

    add vc-task pluing

--- .config/nvim/lua/david/plugins/telescope.lua
+++ .config/nvim/lua/david/plugins/telescope.lua
@@ -138,6 +138,81 @@ return {
     "nvim-telescope/telescope-media-files.nvim",
     opts = {}
 
+  },
+  {
+    "EthanJWright/vs-tasks.nvim",
+    dependencies = {
+      "nvim-lua/popup.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      cache_json_conf = true,  -- don't read the json conf every time a task is ran
+      cache_strategy = "last", -- can be "most" or "last" (most used / last used)
+      config_dir = ".vscode",  -- directory to look for tasks.json and launch.json
+      use_harpoon = false,     -- use harpoon to auto cache terminals
+      telescope_keys = {       -- change the telescope bindings used to launch tasks
+        vertical = '<C-v>',
+        split = '<C-p>',
+        tab = '<C-t>',
+        current = '<CR>',
+      },
+      autodetect = { -- auto load scripts
+        npm = "on"
+      },
+      terminal = 'toggleterm',
+      term_opts = {
+        vertical = {
+          direction = "vertical",
+          size = "80"
+        },
+        horizontal = {
+          direction = "horizontal",
+          size = "10"
+        },
+        current = {
+          direction = "float",
+        },
+        tab = {
+          direction = 'tab',
+        }
+      },
+      json_parser = 'vim.fn.json.decode'
+    },
+    config = function(_, opts)
+      require('telescope').load_extension('vstask')
+      require("vstask").setup(opts)
+    end,
+    keys = {
+      {
+        "<leader>to",
+        function()
+          require("telescope").extensions.vstask.tasks()
+        end,
+        desc = "open task list in telescope"
+      },
+      {
+        "<leader>ti",
+        function()
+          require("telescope").extensions.vstask.inputs()
+        end,
+        desc = "open the input list, set new input"
+      },
+      {
+        "<leader>th",
+        function()
+          require("telescope").extensions.vstask.history()
+        end,
+        desc = "search history of tasks"
+      },
+      {
+        "<leader>tc",
+        function()
+          require("telescope").extensions.vstask.close()
+        end,
+        desc = "close the task runner (if toggleterm)"
+      }
+    }
   }
 
 
