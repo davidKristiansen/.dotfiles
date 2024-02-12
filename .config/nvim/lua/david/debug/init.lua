948fe179aa5065ef23a9efa3771d7cb07be34f9a
David Kristiansen <david.kristiansen@sony.com>
2024-02-12 18:17:16 +0100

    add locals() debug

--- .config/nvim/lua/david/debug/init.lua
+++ .config/nvim/lua/david/debug/init.lua
@@ -26,4 +26,20 @@ function M.tprint (tbl, indent)
   return toprint
 end
 
+function M.locals()
+  local variables = {}
+  local idx = 1
+  while true do
+    local ln, lv = debug.getlocal(2, idx)
+    if ln ~= nil then
+      variables[ln] = lv
+    else
+      break
+    end
+    idx = 1 + idx
+  end
+  return variables
+end
+
+
 return M
