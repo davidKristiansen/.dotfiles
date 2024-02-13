21197bfcc196d903005fa1cbb5f6325828c3cb76
David Kristiansen <david.kristiansen@sony.com>
2024-02-13 16:22:43 +0100

    add rainbows

--- .config/nvim/lua/david/plugins/ui.lua
+++ .config/nvim/lua/david/plugins/ui.lua
@@ -1,36 +1,60 @@
 -- local dragon_colors = require("kanagawa.colors").setup({ theme = 'dragon' })
 
+
+
 return {
-
-
-  {
-    'vimpostor/vim-tpipeline',
-    event = "VeryLazy"
-  },
-
   {
     "luukvbaal/statuscol.nvim",
     event = "VeryLazy",
     opts = {}
   },
-
   {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      {
+        "https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git"
+      }
+    },
     main = "ibl",
     opts = {
-      -- indent = {
-      --   highlight = dragon_colors.dragonBlack3 ,
-      --   -- char = '┊',
-      -- },
-    --   whitespace = {
-    --     highlight = dragon_colors.dragonWhite,
-    --     remove_blankline_trail = false,
-    -- },
-    -- scope = { enabled = true, highlight = dragon_colors.dragonRed },
-    }
-  },
+      indent = {
+        char = '▏',
+      },
+      scope = {
+        show_exact_scope = false,
+        show_start = false
+      }
+    },
+    config = function(_, opts)
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+      local palette = require('gruvbox').palette
+      local hooks = require "ibl.hooks"
 
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = palette.neutral_red })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = palette.neutral_yellow })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = palette.neutral_blue })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = palette.neutral_orange })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = palette.neutral_green })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = palette.neutral_purple })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = palette.neutral_aqua })
+      end)
+
+      vim.g.rainbow_delimiters = { highlight = highlight }
+
+      require("ibl").setup(vim.tbl_deep_extend("force", { scope = { highlight = highlight } }, opts))
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end
+  },
   {
     'RRethy/vim-illuminate',
     event = { "BufReadPre", "BufNewFile" },
@@ -44,17 +68,4 @@ return {
       require('illuminate').configure(opts)
     end
   },
-
-
-
-  -- {
-  --   "xiyaowong/transparent.nvim",
-  --   lazy = false,
-  --   config = function ()
-  --     require("transparent").setup()
-  --     -- require('transparent').clear_prefix('lualine')
-  --   end
-  --
-  -- }
-
 }
