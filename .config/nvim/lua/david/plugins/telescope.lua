31a38f15e3d9312d012dfe212fde7a426c812b00
David Kristiansen <david.kristiansen@sony.com>
2024-02-13 19:45:51 +0100

    add lsp reference keymap

--- .config/nvim/lua/david/plugins/telescope.lua
+++ .config/nvim/lua/david/plugins/telescope.lua
@@ -73,6 +73,7 @@ return {
       { "<leader>sM", "<cmd>Telescope man_pages<cr>",                                    desc = "Man Pages" },
       { "<leader>sm", "<cmd>Telescope marks<cr>",                                        desc = "Jump to Mark" },
       { "<leader>so", "<cmd>Telescope vim_options<cr>",                                  desc = "Options" },
+      { "<leader>sr", "<cmd>Telescope lsp_references<cr>",                               desc = "References" },
       { "<leader>sR", "<cmd>Telescope resume<cr>",                                       desc = "Resume" },
       { "<leader>sw", Util.telescope("grep_string", { word_match = "-w" }),              desc = "Word (root dir)" },
       { "<leader>sW", Util.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)" },
