296afc28badb405c20656f6c7b4d2bb7150de7cf
David Kristiansen <david.kristiansen@sony.com>
2024-02-14 16:11:50 +0100

    telescope git branches

--- .config/nvim/lua/david/plugins/telescope.lua
+++ .config/nvim/lua/david/plugins/telescope.lua
@@ -42,6 +42,7 @@ return {
       -- git
       { "<leader>gc", "<cmd>Telescope git_commits<CR>",                     desc = "commits" },
       { "<leader>gs", "<cmd>Telescope git_status<CR>",                      desc = "status" },
+      { "<leader>gB", "<cmd>Telescope git_branches<CR>",                    desc = "status" },
       -- search
       { '<leader>s"', "<cmd>Telescope registers<cr>",                       desc = "Registers" },
       { "<leader>sa", "<cmd>Telescope autocommands<cr>",                    desc = "Auto Commands" },
