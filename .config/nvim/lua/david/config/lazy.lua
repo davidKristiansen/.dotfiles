8c50264334493ab9ca7b8a7c471202a6ad8e0260
David Kristiansen <david.kristiansen@sony.com>
2024-07-24 10:50:19 +0200

    update lazy.nvim to support luarocks

--- .config/nvim/lua/david/config/lazy.lua
+++ .config/nvim/lua/david/config/lazy.lua
@@ -12,6 +12,20 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+   pkg = {
+    enabled = true,
+    cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
+    -- the first package source that is found for a plugin will be used.
+    sources = {
+      "lazy",
+      "rockspec",
+      "packspec",
+    },
+  },
+  rocks = {
+    root = vim.fn.stdpath("data") .. "/lazy-rocks",
+    server = "https://nvim-neorocks.github.io/rocks-binaries/",
+  },
   spec = {
     import = "david.plugins",
   },
