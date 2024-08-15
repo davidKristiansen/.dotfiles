46ddc015a40519f70767f646ad2b7d56685090ae
David Kristiansen <David.Kristiansen@sony.com>
2024-08-15 10:29:50 +0000

    not sure what this does

--- .config/nvim/lua/david/plugins/indent_blankline.lua
+++ .config/nvim/lua/david/plugins/indent_blankline.lua
@@ -14,8 +14,8 @@ return {
       },
       scope = {
         show_exact_scope = false,
-        show_start = false,
-        show_end = false
+        show_start = true,
+        show_end = true
       }
     },
     config = function(_, opts)
