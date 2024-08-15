e46893d361f78528cd1d5ed34756c20d5dc1f24f
David Kristiansen <David.Kristiansen@sony.com>
2024-08-15 10:29:30 +0000

    add indentscope

--- .config/nvim/lua/david/plugins/mini/init.lua
+++ .config/nvim/lua/david/plugins/mini/init.lua
@@ -13,12 +13,12 @@ return {
       -- require('mini.diff').setup()
       require('mini.extra').setup()
       -- require('mini.git').setup()
-      require('mini.hipatterns').setup()
+      -- require('mini.hipatterns').setup()
       require('mini.icons').setup()
-      -- require('mini.indentscope').setup({
-      --   symbol = '▏',
-      --   options = { try_as_border = true },
-      -- })
+      require('mini.indentscope').setup({
+        symbol = '▏',
+        options = { try_as_border = true },
+      })
       require('mini.jump').setup()
       require('mini.jump2d').setup()
       require('mini.map').setup()
