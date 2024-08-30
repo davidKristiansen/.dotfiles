8b9c2c6a71c994088ef7a587c7d42f62d93babc8
David Kristiansen@ <David.Kristiansen@sony.com>
2024-08-30 12:44:10 +0200

    add swaylock.sh script

---
+++ .local/bin/swaylock.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+swaylock                     \
+  --screenshots              \
+  --clock                    \
+  --indicator                \
+  --indicator-radius 100     \
+  --indicator-thickness 7    \
+  --effect-blur 7x5          \
+  --effect-pixelate 10       \
+  --effect-vignette 0.5:0.5  \
+  --ring-color b8bb26        \
+  --key-hl-color 458588      \
+  --line-color 00000000      \
+  --inside-color 1d2021ef    \
+  --separator-color 00000000 \
+  --grace 15                 \
+  --fade-in 10
