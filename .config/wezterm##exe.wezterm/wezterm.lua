28bfcada7fe20e530b676c4d7bb529826988635a
David Kristiansen@ <David.Kristiansen@sony.com>
2024-09-17 15:42:51 +0200

    stuff

--- .config/wezterm##exe.wezterm/wezterm.lua
+++ .config/wezterm##exe.wezterm/wezterm.lua
@@ -15,7 +15,7 @@ end)
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono'
 config.animation_fps = 10
--- config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = true
 
 
 config.window_padding = {
@@ -25,69 +25,69 @@ config.window_padding = {
   bottom = 0,
 }
 
-config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
-config.keys = {
-  { key = 'l', mods = 'ALT|SHIFT', action = wezterm.action.ShowLauncher },
-  {
-    key = '%',
-    mods = 'LEADER|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
-  },
-  {
-    key = '"',
-    mods = 'LEADER|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-  },
-  {
-    key = 'r',
-    mods = 'LEADER',
-    action = action.ActivateKeyTable {
-      name = 'resize_pane',
-      one_shot = false,
-    },
-  },
-}
+-- config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+-- config.keys = {
+--   { key = 'l', mods = 'ALT|SHIFT', action = wezterm.action.ShowLauncher },
+--   {
+--     key = '%',
+--     mods = 'LEADER|SHIFT',
+--     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+--   },
+--   {
+--     key = '"',
+--     mods = 'LEADER|SHIFT',
+--     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+--   },
+--   {
+--     key = 'r',
+--     mods = 'LEADER',
+--     action = action.ActivateKeyTable {
+--       name = 'resize_pane',
+--       one_shot = false,
+--     },
+--   },
+-- }
 
-for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'ALT',
-    action = action.ActivateTab(i - 1),
+-- for i = 1, 9 do
+--   table.insert(config.keys, {
+--     key = tostring(i),
+--     mods = 'ALT',
+--     action = action.ActivateTab(i - 1),
+--
+--   })
+-- end
 
-  })
-end
-
-config.key_tables = {
-  -- Defines the keys that are active in our resize-pane mode.
-  -- Since we're likely to want to make multiple adjustments,
-  -- we made the activation one_shot=false. We therefore need
-  -- to define a key assignment for getting out of this mode.
-  -- 'resize_pane' here corresponds to the name="resize_pane" in
-  -- the key assignments above.
-  resize_pane = {
-    { key = 'LeftArrow',  action = action.AdjustPaneSize { 'Left', 1 } },
-    { key = 'h',          action = action.AdjustPaneSize { 'Left', 1 } },
-    { key = 'RightArrow', action = action.AdjustPaneSize { 'Right', 1 } },
-    { key = 'l',          action = action.AdjustPaneSize { 'Right', 1 } },
-    { key = 'UpArrow',    action = action.AdjustPaneSize { 'Up', 1 } },
-    { key = 'k',          action = action.AdjustPaneSize { 'Up', 1 } },
-    { key = 'DownArrow',  action = action.AdjustPaneSize { 'Down', 1 } },
-    { key = 'j',          action = action.AdjustPaneSize { 'Down', 1 } },
-    { key = 'LeftArrow',  action = action.AdjustPaneSize { 'Left', 10 },  mods = 'SHIFT', },
-    { key = 'h',          action = action.AdjustPaneSize { 'Left', 10 },  mods = 'SHIFT', },
-    { key = 'RightArrow', action = action.AdjustPaneSize { 'Right', 10 }, mods = 'SHIFT', },
-    { key = 'l',          action = action.AdjustPaneSize { 'Right', 10 }, mods = 'SHIFT', },
-    { key = 'UpArrow',    action = action.AdjustPaneSize { 'Up', 10 },    mods = 'SHIFT' },
-    { key = 'k',          action = action.AdjustPaneSize { 'Up', 10 },    mods = 'SHIFT' },
-    { key = 'DownArrow',  action = action.AdjustPaneSize { 'Down', 10 },  mods = 'SHIFT', },
-    { key = 'j',          action = action.AdjustPaneSize { 'Down', 10 },  mods = 'SHIFT', },
-    { key = 'Escape',     action = 'PopKeyTable' },
-  },
-
-  -- Defines the keys that are active in our activate-pane mode.
-  -- 'activate_pane' here corresponds to the name="activate_pane" in
-  -- the key assignments above.
-}
+-- config.key_tables = {
+--   -- Defines the keys that are active in our resize-pane mode.
+--   -- Since we're likely to want to make multiple adjustments,
+--   -- we made the activation one_shot=false. We therefore need
+--   -- to define a key assignment for getting out of this mode.
+--   -- 'resize_pane' here corresponds to the name="resize_pane" in
+--   -- the key assignments above.
+--   resize_pane = {
+--     { key = 'LeftArrow',  action = action.AdjustPaneSize { 'Left', 1 } },
+--     { key = 'h',          action = action.AdjustPaneSize { 'Left', 1 } },
+--     { key = 'RightArrow', action = action.AdjustPaneSize { 'Right', 1 } },
+--     { key = 'l',          action = action.AdjustPaneSize { 'Right', 1 } },
+--     { key = 'UpArrow',    action = action.AdjustPaneSize { 'Up', 1 } },
+--     { key = 'k',          action = action.AdjustPaneSize { 'Up', 1 } },
+--     { key = 'DownArrow',  action = action.AdjustPaneSize { 'Down', 1 } },
+--     { key = 'j',          action = action.AdjustPaneSize { 'Down', 1 } },
+--     { key = 'LeftArrow',  action = action.AdjustPaneSize { 'Left', 10 },  mods = 'SHIFT', },
+--     { key = 'h',          action = action.AdjustPaneSize { 'Left', 10 },  mods = 'SHIFT', },
+--     { key = 'RightArrow', action = action.AdjustPaneSize { 'Right', 10 }, mods = 'SHIFT', },
+--     { key = 'l',          action = action.AdjustPaneSize { 'Right', 10 }, mods = 'SHIFT', },
+--     { key = 'UpArrow',    action = action.AdjustPaneSize { 'Up', 10 },    mods = 'SHIFT' },
+--     { key = 'k',          action = action.AdjustPaneSize { 'Up', 10 },    mods = 'SHIFT' },
+--     { key = 'DownArrow',  action = action.AdjustPaneSize { 'Down', 10 },  mods = 'SHIFT', },
+--     { key = 'j',          action = action.AdjustPaneSize { 'Down', 10 },  mods = 'SHIFT', },
+--     { key = 'Escape',     action = 'PopKeyTable' },
+--   },
+--
+--   -- Defines the keys that are active in our activate-pane mode.
+--   -- 'activate_pane' here corresponds to the name="activate_pane" in
+--   -- the key assignments above.
+-- }
 
 config.unix_domains = {
   {
