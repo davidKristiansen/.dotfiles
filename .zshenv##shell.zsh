a9b79e299446ad4cd361fc286f9c37d447dc2965
David Kristiansen@ <David.Kristiansen@sony.com>
2024-10-03 14:48:09 +0000

    add devcontainer specific config

--- .zshenv##shell.zsh
+++ .zshenv##shell.zsh
@@ -3,3 +3,13 @@ ZDOTDIR=$HOME/.config/zsh
 if [[ -f "${ZDOTDIR}/.zshenv" ]]; then
   . "${ZDOTDIR}/.zshenv"
 fi
+#
+###############################################################################
+# Load user environment variables
+devcontainer_settings=$HOME/.config/devcontainer_environment/environment_variables
+if [[ -f $devcontainer_settings && -f /.dockerenv ]]; then
+    # Executing in docker - load user specific container settings
+    set -a
+    . $devcontainer_settings
+    set +a
+fi
