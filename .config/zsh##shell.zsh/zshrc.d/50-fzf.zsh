5d296a5d081a2c0faa911c4e8ee7a5caaa351ef8
David Kristiansen <David.Kristiansen@sony.com>
2024-12-22 20:29:04 +0100

    Widgets!

--- .config/zsh##shell.zsh/zshrc.d/50-fzf.zsh
+++ .config/zsh##shell.zsh/zshrc.d/50-fzf.zsh
@@ -1,12 +1,1 @@
-export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*,.venv/*}"'
-
-
-function find_f () {
-  nvim -c ':FzfLua files'
-}
-
-zvm_define_widget find_f
-
-bindkey -M vicmd '^f' find_f
-
-# bindkey '^e' find_f
+FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*,.venv/*}"'
