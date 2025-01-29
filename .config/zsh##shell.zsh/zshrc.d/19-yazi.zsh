99af7684a8de4f4136bf676460f7bf8c618612c7
David Kristiansen <David.Kristiansen@sony.com>
2025-01-29 15:59:53 +0100

    add yazi

---
+++ .config/zsh##shell.zsh/zshrc.d/19-yazi.zsh
@@ -0,0 +1,15 @@
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+  clear
+  eza -TL 1 --icons=always
+}
+
+MAGIC_ENTER_GIT_COMMAND=y
+MAGIC_ENTER_OTHER_COMMAND=y
+
+
