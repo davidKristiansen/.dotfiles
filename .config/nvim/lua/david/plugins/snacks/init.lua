2e7c779f47aa898225fde480eb41ba2701607eeb
David Kristiansen <David.Kristiansen@sony.com>
2025-02-01 20:55:57 +0100

    [nvim] smart search

--- .config/nvim/lua/david/plugins/snacks/init.lua
+++ .config/nvim/lua/david/plugins/snacks/init.lua
@@ -98,7 +98,7 @@ return {
       { "<leader>,",       function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
       { "<leader>/",       function() Snacks.picker.grep() end,                                    desc = "Grep" },
       { "<leader>:",       function() Snacks.picker.command_history() end,                         desc = "Command History" },
-      { "<leader><space>", function() Snacks.picker.files() end,                                   desc = "Find Files" },
+      { "<leader><space>", function() Snacks.picker.smart() end,                                   desc = "Smart finder" },
       -- find
       { "<leader>fb",      function() Snacks.picker.buffers() end,                                 desc = "Buffers" },
       { "<leader>fc",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
