222f60315f717db26aa97077aa1c29cfade3ac11
David Kristiansen <david.kristiansen@sony.com>
2025-02-01 19:54:51 +0000

    [nvim] tweak dashboard

--- .config/nvim/lua/david/plugins/snacks/init.lua
+++ .config/nvim/lua/david/plugins/snacks/init.lua
@@ -45,16 +45,16 @@ return {
         },
         sections = {
           { section = "header" },
-          {
-            pane = 2,
-            section = "terminal",
-            cmd = "colorscript_square",
-            height = 5,
-            padding = 1,
-          },
+          -- {
+          --   pane = 2,
+          --   section = "terminal",
+          --   cmd = "onefetch --no-title --no-art",
+          --   height = 5,
+          --   padding = 1,
+          -- },
           { section = "keys", gap = 1, padding = 1 },
-          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          -- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          -- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           {
             pane = 2,
             icon = " ",
@@ -63,8 +63,9 @@ return {
             enabled = function()
               return Snacks.git.get_root() ~= nil
             end,
-            cmd = "git status --short --branch --renames",
-            height = 5,
+            -- cmd = "git status --short --branch --renames",
+            cmd = "onefetch --no-title --no-art",
+            height = 20,
             padding = 1,
             ttl = 5 * 60,
             indent = 3,
