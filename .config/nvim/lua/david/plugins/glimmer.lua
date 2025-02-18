aba73a6fd047ee4d6305241279421984d82d6527
David Kristiansen <David.Kristiansen@sony.com>
2025-02-18 15:50:58 +0100

    [nvim] some glitter

---
+++ .config/nvim/lua/david/plugins/glimmer.lua
@@ -0,0 +1,35 @@
+return {
+  "rachartier/tiny-glimmer.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration
+    disable_warnings = false,
+    animations = {
+      fade = {
+        from_color = "#FF757F",
+      },
+      reverse_fade = {
+        from_color = "#4FD6BE",
+      },
+    },
+    presets = {
+      pulsar = {
+        enabled = true,
+      },
+    },
+    overwrite = {
+      search = {
+        enabled = false,
+      },
+      paste = {
+        enabled = true,
+      },
+      undo = {
+        enabled = true,
+      },
+      redo = {
+        enabled = true,
+      },
+    },
+  },
+}
