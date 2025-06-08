6f28b84943866e8dbeaaad6ef096b9d94cea4d07
David Kristiansen <david@kristiansen.tech>
2025-06-08 20:32:55 +0000

    [nvim] fix avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -16,12 +16,16 @@ return {
   config = function()
     require("avante").setup({
       provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",
-        timeout = 30000,
-        temperature = 0,
-        max_completion_tokens = 8192,
+      providers = {
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o",
+          timeout = 30000,
+          extra_request_body = {
+            temperature = 0,
+            max_completion_tokens = 8192,
+          },
+        },
       },
     })
   end,
