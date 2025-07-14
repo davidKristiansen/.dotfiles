98fd76489daec8248a8e786863c5170913c4df42
David Kristiansen <david@kristiansen.tech>
2025-07-14 17:54:12 +0200

    [nvim] fix some keymaps

--- .config/nvim/lua/plugins/snacks/keymaps.lua
+++ .config/nvim/lua/plugins/snacks/keymaps.lua
@@ -6,23 +6,24 @@
 ---@type LazyKeys[]
 return {
   -- Top Pickers & Explorer
-  { "<leader>e",       function() Snacks.explorer() end,                       desc = "File Explorer" },
+  { "<leader>e",  function() Snacks.explorer() end,                desc = "File Explorer" },
 
   -- Git
-  { "<leader>gB",      function() Snacks.gitbrowse() end,                      desc = "Git Browse",               mode = { "n", "v" } },
-  { "<leader>gg",      function() Snacks.lazygit() end,                        desc = "Lazygit" },
+  { "<leader>gB", function() Snacks.gitbrowse() end,               desc = "Git Browse",               mode = { "n", "v" } },
+  { "<leader>gg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
 
   -- Other Snacks custom actions
-  { "<leader>z",       function() Snacks.zen() end,                            desc = "Toggle Zen Mode" },
-  { "<leader>Z",       function() Snacks.zen.zoom() end,                       desc = "Toggle Zoom" },
-  { "<leader>.",       function() Snacks.scratch() end,                        desc = "Toggle Scratch Buffer" },
-  { "<leader>S",       function() Snacks.scratch.select() end,                 desc = "Select Scratch Buffer" },
-  { "<leader>bd",      function() Snacks.bufdelete() end,                      desc = "Delete Buffer" },
-  { "<leader>cR",      function() Snacks.rename.rename_file() end,             desc = "Rename File" },
-  { "<leader>un",      function() Snacks.notifier.hide() end,                  desc = "Dismiss All Notifications" },
-  { "<c-/>",           function() Snacks.terminal.toggle() end,                desc = "Toggle Terminal" },
-  { "]]",              function() Snacks.words.jump(vim.v.count1) end,         desc = "Next Reference",           mode = { "n", "t" } },
-  { "[[",              function() Snacks.words.jump(-vim.v.count1) end,        desc = "Prev Reference",           mode = { "n", "t" } },
+  { "<leader>n",  function() Snacks.picker.notifications() end,    desc = "Notification History" },
+  { "<leader>z",  function() Snacks.zen() end,                     desc = "Toggle Zen Mode" },
+  { "<leader>Z",  function() Snacks.zen.zoom() end,                desc = "Toggle Zoom" },
+  { "<leader>.",  function() Snacks.scratch() end,                 desc = "Toggle Scratch Buffer" },
+  { "<leader>S",  function() Snacks.scratch.select() end,          desc = "Select Scratch Buffer" },
+  { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
+  { "<leader>cR", function() Snacks.rename.rename_file() end,      desc = "Rename File" },
+  { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
+  { "<c-/>",      function() Snacks.terminal.toggle() end,         desc = "Toggle Terminal" },
+  { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",           mode = { "n", "t" } },
+  { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",           mode = { "n", "t" } },
 
   -- News (custom window)
   {
@@ -44,4 +45,3 @@ return {
     end,
   },
 }
-
