246c6ad3d9ddab05ab268eabd6e1ffb50196731f
David Kristiansen <david@kristiansen.tech>
2025-08-12 14:23:49 +0000

    fix auto venv

--- .config/zsh##shell.zsh/zshrc.d/00-mise.zsh
+++ .config/zsh##shell.zsh/zshrc.d/00-mise.zsh
@@ -2,5 +2,4 @@
 # Early activation of mise to ensure PATH/shims
 
 # Activate mise (core + shims)
-eval "$("$HOME/.local/bin/mise" activate zsh)"
-eval "$(mise activate --shims)"
+eval "$("$HOME/.local/bin/mise" activate zsh --shims)"
