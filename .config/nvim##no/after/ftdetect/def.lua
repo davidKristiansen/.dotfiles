2cbd4b1c0bf3e5c894e0e3987c69311cb2dbef7d
David Kristiansen <david@kristiansen.tech>
2025-10-01 11:51:35 +0000

    fix(sys): more fixes

--- .config/nvim##no/after/ftdetect/def.lua
+++
@@ -1,7 +0,0 @@
--- Recognize *.def as "def" (our custom filetype name)
-vim.filetype.add({
-  extension = {
-    def = "def",
-  },
-})
-
--- .config/nvim##no/lua/lsp/ty.lua
+++
@@ -1,9 +0,0 @@
--- SPDX-License-Identifier: MIT
--- Copyright David Kristiansen
-
-return {
-  name = "ty",
-  cmd = { "ty", "server" },
-  filetypes = { "python" },
-  root_markers = { "pyproject.toml", ".git", ".venv", "ty.toml" }
-}
