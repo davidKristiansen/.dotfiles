1aa24a5fc1d1c019b01fea6abb6371f62dfc770b
david@kristiansen.tech <david@kristiansen.tech>
2025-11-06 22:07:56 +0100

    feat(nvim): lsp fixes

--- .config/nvim/lua/core/lsp/init.lua
+++ .config/nvim/lua/core/lsp/init.lua
@@ -1,6 +1,16 @@
+-- init.lua
 -- SPDX-License-Identifier: MIT
 
 vim.api.nvim_create_autocmd("LspAttach", {
   desc = "LSP actions",
-  callback = require "core.lsp.on_attach"
+  callback = require("core.lsp.on_attach"),
+})
+
+require("core.lsp.fix_all").setup({
+  enable_on_save = true,
+  filetypes = { "python" },
+  create_commands = true,
+  create_mappings = true,
+  mapping_run = "<leader>fa",
+  mapping_toggle = "<leader>ta",
 })
--- .config/nvim/lua/core/lsp/on_attach.lua
+++ .config/nvim/lua/core/lsp/on_attach.lua
@@ -1,64 +1,39 @@
+-- lua/core/lsp/on_attach.lua
 -- SPDX-License-Identifier: MIT
 
-
-vim.g.format_on_save = true
-vim.g.inlay_hints_enabled = false
+vim.g.format_on_save             = true
+vim.g.inlay_hints_enabled        = false
 vim.g.on_type_formatting_enabled = true
+vim.g.fix_all_on_save            = false
 
-local keymaps = require "core.lsp.keymaps"
+local keymaps                    = require "core.lsp.keymaps"
+local fixall                     = require "core.lsp.fix_all"
 
--- Keymaps for LSP features.
--- See `:help vim.lsp.buf` for more information.
 return function(args)
   local bufnr = args.buf
   local client = vim.lsp.get_client_by_id(args.data.client_id)
-
   if not client then return end
 
   keymaps.setup(bufnr)
 
-  if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
-    if vim.g.inlay_hints_enabled == nil then
-      vim.g.inlay_hints_enabled = true
-    end
-    if vim.g.inlay_hints_enabled then
-      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-    end
+  if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr)
+      and vim.g.inlay_hints_enabled then
+    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
   end
 
-  if client:supports_method(vim.lsp.protocol.Methods.textDocument_onTypeFormatting, bufnr) then
-    if vim.g.on_type_formatting_enabled == nil then
-      vim.g.on_type_formatting_enabled = true
-    end
-    if vim.g.on_type_formatting_enabled then
-      vim.lsp.on_type_formatting.enable(true, { bufnr = bufnr })
-    end
+  if client:supports_method(vim.lsp.protocol.Methods.textDocument_onTypeFormatting, bufnr)
+      and vim.g.on_type_formatting_enabled then
+    vim.lsp.on_type_formatting.enable(true, { bufnr = bufnr })
   end
 
-  if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting, bufnr) then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      callback = function()
-        if not vim.g.format_on_save then return end
-        vim.lsp.buf.format { async = false }
-      end,
-    })
-  end
+  -- install the pipeline for this buffer too (harmless if you also call setup())
+  fixall.attach(bufnr)
 
   if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
     vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
-
-    vim.keymap.set(
-      'i',
-      '<C-F>',
-      vim.lsp.inline_completion.get,
-      { desc = 'LSP: accept inline completion', buffer = bufnr }
-    )
-    vim.keymap.set(
-      'i',
-      '<C-G>',
-      vim.lsp.inline_completion.select,
-      { desc = 'LSP: switch inline completion', buffer = bufnr }
-    )
+    vim.keymap.set('i', '<C-F>', vim.lsp.inline_completion.get,
+      { desc = 'LSP: accept inline completion', buffer = bufnr })
+    vim.keymap.set('i', '<C-G>', vim.lsp.inline_completion.select,
+      { desc = 'LSP: switch inline completion', buffer = bufnr })
   end
 end
