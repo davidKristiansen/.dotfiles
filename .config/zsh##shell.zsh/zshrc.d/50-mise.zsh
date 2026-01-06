2bda30e2ff8efcce40cbdcfcb403cbedc3881d91
David Kristiansen <david@kristiansen.tech>
2026-01-06 10:37:33 +0100

    feat(zsh): mise use shims

--- .config/zsh##shell.zsh/zshrc.d/50-mise.zsh
+++ .config/zsh##shell.zsh/zshrc.d/50-mise.zsh
@@ -10,10 +10,10 @@ fi
 
 # Activate mise
 if command -v mise >/dev/null 2>&1; then
-  eval "$(mise activate zsh)"
+  eval "$(mise activate zsh --shims)"
 elif [[ -x "$HOME/.local/bin/mise" ]]; then
   # Fallback if not in PATH
-  eval "$($HOME/.local/bin/mise activate zsh)"
+  eval "$($HOME/.local/bin/mise activate zsh --shims)"
 fi
 
 # vim: set ft=zsh ts=2 sw=2:
