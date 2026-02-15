2f87a3d094b07272519a8e04705fc14e8c526072
David Kristiansen <david@kristiansen.tech>
2026-02-15 14:28:16 +0100

    fix(nvim): remove usage of removed vim._extui module

--- .config/nvim/lua/plugins/blink_pairs.lua
+++ .config/nvim/lua/plugins/blink_pairs.lua
@@ -3,7 +3,7 @@ vim.pack.add({
   { src = "https://github.com/saghen/blink.pairs",   version = vim.version.range("*") },
 }, { confirm = false })
 
-require('vim._extui').enable({})
+-- require('vim._extui').enable({})
 
 require("blink.pairs").setup({
   highlights = {
