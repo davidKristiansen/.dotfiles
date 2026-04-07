d0e57e4544c7d85c0e25206bb937ba72de65ff7f
David Kristiansen <david@kristiansen.tech>
2026-04-07 11:33:37 +0000

    fix(nvim): set tpipeline options before lazy-loading to avoid statusline flash
    Move tpipeline_autoembed and laststatus=0 before vim.schedule so they
    take effect immediately on startup.

--- .config/nvim/plugin/tmux.lua
+++ .config/nvim/plugin/tmux.lua
@@ -3,6 +3,9 @@
 
 if not vim.env.TMUX then return end
 
+vim.g.tpipeline_autoembed = 0
+vim.o.laststatus = 0   -- hide nvim statusline immediately; tpipeline pipes it to tmux
+
 vim.schedule(function()
   vim.pack.add({
     { src = 'https://github.com/alexghergh/nvim-tmux-navigation' },
@@ -12,6 +15,4 @@ vim.schedule(function()
   require('nvim-tmux-navigation').setup({
     disable_when_zoomed = true,
   })
-
-  vim.g.tpipeline_autoembed = 0
 end)
